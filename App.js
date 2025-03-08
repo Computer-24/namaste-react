fce1d464bf8483171b11ef95160461475442484c
Your Name <you@example.com>
2025-03-08 19:53:58 +0300

    "end of episode 2"

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement(
     "div",
     {id: "parent"},
@@ -5,16 +8,16 @@ const parent = React.createElement(
         "div",
         {id: "child"},
         [
-            React.createElement("h1", {}, "I am h1 tag"),
-            React.createElement("h1", {}, "I am h1 tag2"),
+            React.createElement("h1", {}, "This! is Namaste React and Js Yo"),
+            React.createElement("h1", {}, "The second tag and nice"),
         ]
     ),
         React.createElement(
             "div",
             {id: "child2"},
             [
-                React.createElement("h1", {}, "I am h1 tag3"),
-                React.createElement("h2", {}, "I am h1 tag4"),
+                React.createElement("h1", {}, "Very nice React and Js Yo"),
+                React.createElement("h1", {}, "I am h1 tag4"),
             ]
         ),]
 )
